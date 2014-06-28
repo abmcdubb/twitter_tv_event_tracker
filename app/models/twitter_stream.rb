fd2ea1021bf1cea7dd8259e92edde4468c0f37cd
abmcdubb <abmcwilliams@gmail.com>
2014-06-28 18:32:12 -0400

    added option for search parameters in twitter stream

--- app/models/twitter_stream.rb
+++ app/models/twitter_stream.rb
@@ -1,35 +1,30 @@
 class TwitterStream
-  
+
   def initialize(*search_terms)
+    TweetStream.configure do |config|  
+      config.consumer_key       = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret    = ENV['TWITTER_CONSUMER_SECRET']
+      config.oauth_token        = ENV['TWITTER_OAUTHTOKEN']
+      config.oauth_token_secret = ENV['TWITTER_OAUTHTOKEN_SECRET']
+      config.auth_method        = :oauth
+    end
+
+    @client = TweetStream::Client.new
+
     @search_terms = search_terms
-    binding.pry
+    @search_terms.concat(['world cup','worldcup','worldcupbrazil','worldcup2014','FIFAWorldCup','2014WC_Brazil','copadomundo','LaCopaFIFA', 'FIFA2014'])
   end
-
-  TweetStream.configure do |config|  
-    config.consumer_key       = ENV['TWITTER_CONSUMER_KEY']
-    config.consumer_secret    = ENV['TWITTER_CONSUMER_SECRET']
-    config.oauth_token        = ENV['TWITTER_OAUTHTOKEN']
-    config.oauth_token_secret = ENV['TWITTER_OAUTHTOKEN_SECRET']
-    config.auth_method        = :oauth
-  end
-
-  client = TweetStream::Client.new
-
-  # TweetStream::Client.new.sample do |status|
-  #   # The status object is a special Hash with
-  #   # method access to its keys.
-  #   # puts "#{status.created_at} #{status.text}"
-  #   count = $redis.incr("#{[status.created_at]}")
-  #   puts "#{status.created_at}: #{status.text} #{count}"
-  # end 
 
   # client.on_error do |message|
   #   puts message  
   # end
 
-  client.track('world cup', 'worldcup') do |status|
-    count = $redis.incr("#{[status.created_at]}")
-    puts "#{status.created_at}: #{status.text} #{count}"
+  def start_stream  
+    @client.track(@search_terms.each {|term| print "'#{term} ',"}) do |status|
+      count = $redis.incr("#{[status.created_at]}")
+      puts "#{status.created_at}: #{status.text} #{count}"
+      # puts @search_terms
+    end
   end
 
 end
